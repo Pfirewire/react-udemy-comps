add5c4b987a263ebbf871ca0554b52eb7cc6d371
Stephen Pflug <stephen@pflug.us>
2023-02-10 13:59:51 -0600

    Created and AccordionPage and moved App contents to it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import Accordion from "./components/Accordion";
+import AccordionPage from "./pages/AccordionPage";
+import ButtonPage from "./pages/ButtonPage";
 
 function App() {
-    const items = [
-        {
-            id: 'fdkjsli9',
-            label: 'Can I use React on a project?',
-            content: 'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. ',
-        },
-        {
-            id: 'falf988',
-            label: 'Can I use Javascript on a project?',
-            content: 'You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want. ',
-        },
-        {
-            id: 'a987fds79',
-            label: 'Can I use CSS on a project?',
-            content: 'You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. ',
-        },
-    ];
-
     return(
-        <Accordion items={items} />
+        <div>
+            <AccordionPage />
+            <ButtonPage />
+        </div>
     );
 }
 
