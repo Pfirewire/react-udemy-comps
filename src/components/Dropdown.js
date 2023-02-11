bf0509d17d3fed97eec38cd52cb3270de107895e
Stephen Pflug <stephen@pflug.us>
2023-02-11 06:32:36 -0600

    Checking Click Location

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,13 +4,20 @@ import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
+    const divEl = useRef();
 
     useEffect(() => {
         const handler = (e) => {
-
+            if(!divEl.current) {
+                return;
+            }
+            if(!divEl.current.contains(e.target)) {
+                setIsOpen(false);
+            }
         };
 
         document.addEventListener('click', handler, true);
+        return () => document.removeEventListener('click', handler);
     }, [])
 
     const handleMenuToggle = () => {
@@ -34,7 +41,7 @@ function Dropdown({ options, value, onChange }) {
     });
 
     return(
-        <div className="w-48 relative">
+        <div ref={divEl} className="w-48 relative">
             <Panel
                 className="flex justify-between items-center cursor-pointer"
                 onClick={handleMenuToggle}
