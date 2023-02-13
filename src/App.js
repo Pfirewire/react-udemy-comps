a4d4d7c0b90a0d37cc0702ba15ba847d3e31c857
Stephen Pflug <stephen@pflug.us>
2023-02-13 12:08:11 -0600

    A Route Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import Link from './components/Link';
-
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import ButtonPage from "./pages/ButtonPage";
 
 function App() {
     return(
         <div>
+            <Link to='/accordion'>Go to Accordion</Link>
+            <Link to='/dropdown'>Go to Dropdown</Link>
+            <Link to='/buttons'>Go to Buttons</Link>
+
+            <div>
+                <Route path='/accordion'>
+                    <AccordionPage />
+                </Route>
+                <Route path='/dropdown'>
+                    <DropdownPage />
+                </Route>
+                <Route path='/buttons'>
+                    <ButtonPage />
+                </Route>
+            </div>
         </div>
     );
 }
