e6d8cec8db4d981b7cbdfee6e91317568ae28345
Stephen Pflug <stephen@pflug.us>
2023-02-12 19:00:24 -0600

    Extracted DropdownPage from App.js into own page file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import AccordionPage from "./pages/AccordionPage";
 import ButtonPage from "./pages/ButtonPage";
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
+import DropdownPage from "./pages/DropdownPage";
+
 
 function App() {
-    const [selection, setSelection] = useState(null);
-
-    const handleSelect = (option) => {
-        setSelection(option);
-    };
-
-    const options = [
-        { label: 'Red', value: 'red' },
-        { label: 'Green', value: 'green' },
-        { label: 'Blue', value: 'blue' },
-    ];
-
     return(
         <div>
-            <Dropdown options={options} value={selection} onChange={handleSelect} />
+            <AccordionPage />
+            <ButtonPage />
+            <DropdownPage />
         </div>
     );
 }
