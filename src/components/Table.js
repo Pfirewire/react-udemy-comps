3180193c5735a27b2012d969e217bafa12d636cc
Stephen Pflug <stephen@pflug.us>
2023-02-15 11:58:57 -0600

    Reminder on Table HTML Structure

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,9 +2,18 @@
 
 function Table({ data }) {
     return(
-        <div>
-            {data.length}
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Fruits</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
     );
 }
 
