e7eb4128e600051f226bc8e588fe8d187ef804d1
Stephen Pflug <stephen@pflug.us>
2023-02-15 13:26:35 -0600

    Styling Header Cells

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
 import Table from "./Table";
 
 function SortableTable(props) {
@@ -26,10 +27,16 @@ function SortableTable(props) {
 
         return {
             ...column,
-            header: () => <th onClick={() => handleClick(column.label)}>
-                {getIcons(column.label, sortBy, sortOrder)}
-                {column.label}
-            </th>
+            header: () =>
+                <th
+                    className='cursor-pointer hover:bg-gray-100'
+                    onClick={() => handleClick(column.label)}
+                >
+                    <div className='flex items-center'>
+                        {getIcons(column.label, sortBy, sortOrder)}
+                        {column.label}
+                    </div>
+                </th>
         }
     });
 
@@ -56,14 +63,32 @@ function SortableTable(props) {
 
 function getIcons(label, sortBy, sortOrder) {
     if(label !== sortBy) {
-        return 'Show both icons';
+        return(
+            <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>
+        );
     }
     if(sortOrder === null) {
-        return 'Show both icons';
+        return(
+            <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>
+        );
     } else if(sortOrder === 'asc') {
-        return 'Show up icon';
+        return(
+            <div>
+                <GoArrowSmallUp />
+            </div>
+        );
     } else if(sortOrder === 'desc') {
-        return 'Show down icon';
+        return(
+            <div>
+                <GoArrowSmallDown />
+            </div>
+        );
     }
 }
 
